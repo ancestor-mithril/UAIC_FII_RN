885945a1a8d47b3fcabf7847d0725a3761d7fa1e
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-12-17 17:19:16 +0200

    Modified during RN lab

--- Tema_3/main.py
+++ Tema_3/main.py
@@ -10,9 +10,9 @@ train_set, valid_set, test_set = load_data()
 # to_image(train_set[0][0].reshape([28, 28]))  # testing data augmentation: blurring images
 
 
-train_set = generate_more_training_data(train_set)
+# train_set = generate_more_training_data(train_set)
 
-Network([784, 100, 10]).train_neural_network(train_set, iterations=10, testing_set=test_set, apply_dropout=True,
-                                             dropout_rate=0.2)
+# Network([784, 100, 10]).train_neural_network(train_set, iterations=10, testing_set=test_set, apply_dropout=True,
+#                                              dropout_rate=0.2)
 
-print(check_trained_network("neural_network_with_dropout_rate_20_10_gen.txt", test_set))
+print(check_trained_network("neural_network_trained_on_blurred_data_with_dropout_rate_20_10_gen.txt", test_set))
