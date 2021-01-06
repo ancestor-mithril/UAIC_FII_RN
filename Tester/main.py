3d27746b2e9562467d47954976dec9ac931afacb
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-01-06 11:41:38 +0200

    added work done'

--- Tester/main.py
+++ Tester/main.py
@@ -47,66 +47,82 @@ from keras.models import load_model
 model = load_model('a.h5')
 second_model = load_model('a.h5')
 
-env = gym.make("BreakoutDeterministic-v4")  # is better than v0
+env = gym.make("BreakoutNoFrameskip-v4")  # is better than v0
 while True:  # training continues forever
+    print(iterations)
     observation = env.reset()  # reseting env
     state = rgb_to_greyscale(observation)  # converts observation (210, 160, 3), to greydownscaled state, (105, 80)
+
+    # when predictiong only LEFT or RIGHT, we have to manually FIRE
     env.step(1)  # FIRE triggers ball initialization
+
     episode_reward = 0  # initializes reward for the new episode which follows
+
     while True:  # agent still has lives
         env.render()
+
         iterations += 1  # increasing total iterations of game
         if epsilon > np.random.rand(1)[0] or iterations < 5:  # a leap of fate, exploration
+            # case of model output being only LEFT / RIGHT
             action = 2 if np.random.random(1)[0] > 0.5 else 3  # random left or right | 3 is left, 2 is right
+
+            # case of model output being all 4 actions: NOOP, FIRE, LEFT, RIGHT
+        #             action = np.random.choice(4)
         else:  # agent must predict action, exploatation
             four_states = np.array(state_history[-4:])  # takes last 4 known states
             four_states = four_states.reshape(105, 80, 4)  # reshapes them into input shape
             predictions = model.predict(np.array([four_states]), verbose=0)  # gets reward predictions for both actions
+            # case of model output being only LEFT / RIGHT
             action = 2 if predictions[0, 0] > predictions[
                 0, 1] else 3  # choses the actions with the greatest predicted reward
 
+            # case of model output being all 4 actions: NOOP, FIRE, LEFT, RIGHT
+        #             action = np.argmax(predictions[0])
+
         if epsilon > 0.05:  # decay over time is applied to epsilon until it reaches critical value
-            epsilon -= epsilon / 10000  # * np.random.random(1)   # decrease is done by (at least) 0.01 %, critical value is reached in (at least) 29956 steps
+            epsilon -= epsilon / 1000000  # critical value is reached in 2995732 steps
+
+        # we should be more interested about total reward later in training, at the beggining we should be greedy for reward
+        if gamma < 0.99:
+            gamma += gamma / 1000000
 
         observation, reward, done, info = env.step(
             action)  # action is played, returns new observation, possible reward, done flag and lives remained
         next_state = rgb_to_greyscale(
-            observation)  # converts observation (210, 160, 3), to greydownscaled state, (105, 80)  # TODO: check if next_state is really needed, we might only use state
+            observation)  # converts observation (210, 160, 3), to greydownscaled state, (105, 80)
 
         if info["ale.lives"] != previous_lives:  # if number of lives decreased during this frame
             env.step(1)  # FIRE resummons ball
             previous_lives = info["ale.lives"]  # updates previous_lives with current lives
-            reward -= 10  # updates reward with negative value because a life was lost
+            # here we tried different negative rewards, including no negative reward for life lost and including no negative reward for DONE
+            #             reward -= 1  # updates reward with negative value because a life was lost
+            if done:  # if game is finished, agent lost
+                reward -= 1
 
+        # we also tried this, the purpose being to encourage the agent to try to make the ball reach above bricks and finish the game faster, but it never reached that state
+        # uncomment this later
+        # if reward == 0:  # if no reward is received
+        #     reward -= 0.1  # reward receives small negative value, should encourage the agent to finish the game faster
+
+        # saving values
         state_history.append(state)
         action_history.append(action)
         reward_history.append(reward)
-        next_state_history.append(
-            next_state)  # next_state of state_history[3] = state_history[4]  # TODO: only use state_history
         state = next_state  # replaces old state with new one
 
         episode_reward += reward  # increases reward for this episode, for checking out improvements for games
 
-        # TODO: apply backprop sometimes in the future
+        # Start Back Prop
 
-
-        if len(action_history) > max_memory_legth:  # if max memory was reached
-            del state_history[:5000]  # deletes first 5000 elements from each list
-            del action_history[:5000]
-            del reward_history[:5000]
-            del next_state_history[:5000]
 
         if done:  # end game flag
-
             games_played += 1  # increasing played games
             if games_played % 10 == 0:  # once every 100 played games
-                env.close()
                 exit()
-                # model.save("a.h5")
+                model.save("a1.h5")
                 print(f"Reward: {episode_reward}, games played: {games_played}, iterations made: {iterations}")
             break  # exits current game
 
     if iterations % 10000 == 0:
         print(f"Reward: {episode_reward}, games played: {games_played}, iterations made: {iterations}")
         print(games_played)
-# env.close()
